e66f858300d30da88c9488083fe2e67da2b1dac2
Cellyane Lima <cellyane@gmail.com>
2024-12-17 14:53:59 +1300

    "Fix bugs"

---
+++ client/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Made with ❤️ to find your movie!</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ client/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>Movies</h1>
+      <p>Find your favorite movie: </p>
+    </header>
+  )
+}
